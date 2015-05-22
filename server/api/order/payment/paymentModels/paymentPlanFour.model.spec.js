2a7b81c474634ebf0103ba5138ad8d49a234cbbb
Ricardo Lara <riclara@gmail.com>
2015-05-22 14:26:01 -0500

    model one

--- server/api/order/payment/paymentModels/paymentPlanFour.model.spec.js
+++ server/api/order/payment/paymentModels/paymentPlanFour.model.spec.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var faker = require('faker');
+var moment = require('moment');
+
+var model = {
+  name : faker.name.firstName,
+  price : 1860,
+  basePrice : 1800,
+  intervalNumber : 6,
+  deposit : 0,
+  feePrice : 10,
+  feeNumber : 6,
+  dateStart : moment("01-06-2015", "DD-MM-YYYY"),
+  dateDeposit : null,
+  dateInterval : 1,
+  dateFirstPayment : moment("01-06-2015", "DD-MM-YYYY")
+}
+
+module.exports = model;
