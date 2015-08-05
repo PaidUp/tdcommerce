ab1d5074287556934619755268b4e4a1f62424fb
Ricardo Lara <riclara@gmail.com>
2015-08-05 10:01:56 -0500

    remove log

--- server/api/commerce.service.spec.js
+++ server/api/commerce.service.spec.js
@@ -4,12 +4,10 @@ var config = require('../config/environment/index');
 var service = require('./commerce.service');
 var assert = require('chai').assert;
 
-describe.only('commerce service', function(){
+describe('commerce service', function(){
   it('retry payment' , function(done){
     this.timeout(30000);
     service.retryPayment(function(err, data){
-      //console.log('err',err);
-      console.log('data' ,JSON.stringify(data));
       assert(data);
       done()
     })
