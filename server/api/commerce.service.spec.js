4b108bea9fa35f665a9e4a456a36755e7411dac0
Ricardo Lara <riclara@gmail.com>
2015-09-09 15:40:04 -0500

    add time to test

--- server/api/commerce.service.spec.js
+++ server/api/commerce.service.spec.js
@@ -4,8 +4,8 @@ var config = require('../config/environment/index');
 var service = require('./commerce.service');
 var assert = require('chai').assert;
 
-describe('commerce service', function(){
-  this.timeout(30000);
+describe.only('commerce service', function(){
+  this.timeout(100000);
   it('retry payment' , function(done){
     service.retryPayment(function(err, data){
       assert(data);
