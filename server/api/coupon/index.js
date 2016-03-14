63305454d79a3b5cf1dc55ae3790b2e7037329de
Jesse Cogollo <cogollo87@gmail.com>
2016-03-14 14:51:34 -0500

    add interceptor to validate token

--- server/api/coupon/index.js
+++ server/api/coupon/index.js
@@ -2,12 +2,13 @@
 
 let express = require('express');
 let controller = require('./coupon.controller');
+let authServer = require('../auth/auth.service');
 
 let router = express.Router();
 
-router.post('/create', controller.create);
-router.post('/list', controller.list);
-router.put('/update', controller.update);
-router.post('/redeem', controller.redeem);
+router.post('/create', authServer.isAuthenticated(), controller.create);
+router.post('/list', authServer.isAuthenticated(), controller.list);
+router.put('/update', authServer.isAuthenticated(), controller.update);
+router.post('/redeem', authServer.isAuthenticated(), controller.redeem);
 
 module.exports = router;
