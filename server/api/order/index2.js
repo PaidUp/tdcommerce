c99416af5c613efa96b2c22adcd91d074a326222
Jesse Cogollo <cogollo87@gmail.com>
2016-03-17 17:19:08 -0500

    add update payments method

--- server/api/order/index2.js
+++ server/api/order/index2.js
@@ -1,13 +1,14 @@
-'use strict';
+'use strict'
 
-let express = require('express');
-let controller = require('./order.controller');
+let express = require('express')
+let controller = require('./order.controller')
 
-let router = express.Router();
+let router = express.Router()
 
-router.post('/create', controller.create);
-router.post('/list', controller.listV2);
-router.put('/update', controller.update);
-router.post('/add-payments', controller.addPayments);
+router.post('/create', controller.create)
+router.post('/list', controller.listV2)
+router.put('/update', controller.update)
+router.post('/add-payments', controller.addPayments)
+router.post('/update-payments', controller.updatePayments)
 
-module.exports = router;
+module.exports = router
--- server/api/order/order.controller.js
+++ server/api/order/order.controller.js
@@ -1,163 +1,181 @@
-'use strict';
+'use strict'
 
-var _ = require('lodash');
-var commerceService = require('../commerce.service.js');
-var logger = require('../../config/logger');
+var _ = require('lodash')
+var commerceService = require('../commerce.service.js')
+var logger = require('../../config/logger')
 let orderModel = require('./order.model').orderModel
 let orderService = require('./order.service')
 
-exports.create = function(req, res){
-  if(req.body.paymentsPlan && req.body.paymentsPlan.length > 0){
+exports.create = function (req, res) {
+  if (req.body.paymentsPlan && req.body.paymentsPlan.length > 0) {
     req.body.paymentsPlan = orderService.createPayments(req.body.paymentsPlan)
   }
-  orderModel.create(req.body, function(err, order){
-    if (err) return res.status(400).json({err:err})
-    return res.status(200).json({_id:order._id, status: order.status, paymentsPlan: order.paymentsPlan})
+  orderModel.create(req.body, function (err, order) {
+    if (err) return res.status(400).json({err: err})
+    return res.status(200).json({_id: order._id, status: order.status, paymentsPlan: order.paymentsPlan})
   })
 }
 
-exports.listV2 = function(req, res){
-  orderModel.find(req.body, function(err, orders){
-    if (err) return res.status(400).json({err:err})
-    return res.status(200).json({orders:orders})
+exports.listV2 = function (req, res) {
+  orderModel.find(req.body, function (err, orders) {
+    if (err) return res.status(400).json({err: err})
+    return res.status(200).json({orders: orders})
   })
 }
 
-exports.update = function(req, res){
-  //TODO see address.controller.
-  orderModel.update(req.body.filter, {'$set':req.body.data}, function(err, orders){
-    if (err) return res.status(400).json({err:err})
-    return res.status(200).json({orders:orders})
+exports.update = function (req, res) {
+  orderModel.findOneAndUpdate(req.body.filter, {'$set': req.body.data}, function (err, orders) {
+    if (err) return res.status(400).json({err: err})
+    return res.status(200).json({orders: orders})
   })
 }
 
-exports.addPayments = function(req, res){
+exports.addPayments = function (req, res) {
   req.body.paymentsPlan = orderService.createPayments(req.body.paymentsPlan)
-  orderModel.update({_id:req.body.orderId}, {'$push':{paymentsPlan:{ $each:req.body.paymentsPlan}}}, function(err, order){
-    if (err) return res.status(400).json({err:err})
+  orderModel.findOneAndUpdate({_id: req.body.orderId}, {'$push': {paymentsPlan: { $each: req.body.paymentsPlan}}}, function (err, order) {
+    if (err) return res.status(400).json({err: err})
     return res.status(200).json(order)
   })
 }
 
-exports.load = function(req, res) {
-  if(!req.params && !req.params.orderId) {
+exports.updatePayments = function (req, res) {
+  let filter = {_id: req.body.orderId, 'paymentsPlan._id': req.body.paymentPlanId}
+  orderModel.findOneAndUpdate(filter, {'$set': {
+      'paymentsPlan.$.destinationId': req.body.paymentPlan.destinationId,
+      'paymentsPlan.$.dateCharge': req.body.paymentPlan.dateCharge,
+      'paymentsPlan.$.price': req.body.paymentPlan.price,
+      'paymentsPlan.$.typeAccount': req.body.paymentPlan.typeAccount,
+      'paymentsPlan.$.account': req.body.paymentPlan.account,
+      'paymentsPlan.$.discount': req.body.paymentPlan.discount,
+      'paymentsPlan.$.discountCode': req.body.paymentPlan.discountCode,
+      'paymentsPlan.$.wasProcessed': req.body.paymentPlan.wasProcessed,
+      'paymentsPlan.$.status': req.body.paymentPlan.status // TODO add all fields
+  }}, function (err, order) {
+    console.log('err', err)
+    console.log('order', order)
+    if (err) return res.status(400).json({err: err})
+    return res.status(200).json(order)
+  })
+}
+
+exports.load = function (req, res) {
+  if (!req.params && !req.params.orderId) {
     return res.status(400).json({
-      "code": "ValidationError",
-      "message": "Order Id is required"
-    });
+      'code': 'ValidationError',
+      'message': 'Order Id is required'
+    })
   }
-  commerceService.orderLoad(req.params.orderId, function(err, dataService){
-    if(err) return handleError(res, err);
-    return res.status(200).json(dataService);
-  });
+  commerceService.orderLoad(req.params.orderId, function (err, dataService) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(dataService)
+  })
 }
 
-exports.list = function(req, res) {
-  commerceService.orderList(req.body, function(err, dataService){
-    if(err) return handleError(res, err);
-    return res.status(200).json(dataService);
-  });
+exports.list = function (req, res) {
+  commerceService.orderList(req.body, function (err, dataService) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(dataService)
+  })
 }
 
-exports.commentCreate = function(req, res) {
-  commerceService.orderCommentCreate(req.body.orderId, req.body.comment, req.body.status, function(err, dataService){
-    if(err) return handleError(res, err);
-    return res.status(200).json(dataService);
-  });
+exports.commentCreate = function (req, res) {
+  commerceService.orderCommentCreate(req.body.orderId, req.body.comment, req.body.status, function (err, dataService) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(dataService)
+  })
 }
 
-exports.updateStatus = function(req, res) {
-  if(!req.params && !req.params.orderId && !req.params.status) {
+exports.updateStatus = function (req, res) {
+  if (!req.params && !req.params.orderId && !req.params.status) {
     return res.status(400).json({
-      "code": "ValidationError",
-      "message": "Order Id is required"
-    });
+      'code': 'ValidationError',
+      'message': 'Order Id is required'
+    })
   }
-  commerceService.orderUpdateStatus(req.params.orderId, req.params.status, function(err, dataService){
-    if(err) return handleError(res, err);
-    return res.status(200).json(dataService);
-  });
+  commerceService.orderUpdateStatus(req.params.orderId, req.params.status, function (err, dataService) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(dataService)
+  })
 }
 
-exports.retryPayment = function(req, res) {
-  commerceService.retryPayment(function(err, data){
-    if(err) return handleError(res, err);
-    return res.status(200).json(data);
-  });
+exports.retryPayment = function (req, res) {
+  commerceService.retryPayment(function (err, data) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(data)
+  })
 }
 
-exports.complete = function(req, res) {
-  commerceService.completeOrders(function(err, data){
-    if(err) return handleError(res, err);
-    return res.status(200).json(data);
-  });
+exports.complete = function (req, res) {
+  commerceService.completeOrders(function (err, data) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(data)
+  })
 }
 
-exports.createInvoice = function(req, res) {
-  commerceService.completeOrders(function(err, data){
-    if(err) return handleError(res, err);
-    return res.status(200).json(data);
-  });
+exports.createInvoice = function (req, res) {
+  commerceService.completeOrders(function (err, data) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(data)
+  })
 }
 
-exports.createOrderInvoice = function(req, res) {
-  if(!req.params && !req.params.orderId) {
+exports.createOrderInvoice = function (req, res) {
+  if (!req.params && !req.params.orderId) {
     return res.status(400).json({
-      "code": "ValidationError",
-      "message": "Order Id is required"
-    });
+      'code': 'ValidationError',
+      'message': 'Order Id is required'
+    })
   }
-  commerceService.createOrderInvoice(req.params.orderId, function(err, data){
-    if(err) return handleError(res, err);
-    return res.status(200).json(data);
-  });
+  commerceService.createOrderInvoice(req.params.orderId, function (err, data) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(data)
+  })
 }
 
-exports.createOrderCreditMemo = function(req, res) {
-  if(!req.params && !req.params.orderId) {
+exports.createOrderCreditMemo = function (req, res) {
+  if (!req.params && !req.params.orderId) {
     return res.status(400).json({
-      "code": "ValidationError",
-      "message": "Order Id is required"
-    });
+      'code': 'ValidationError',
+      'message': 'Order Id is required'
+    })
   }
-  if(!req.body && !req.body.qty) {
+  if (!req.body && !req.body.qty) {
     return res.status(400).json({
-      "code": "ValidationError",
-      "message": "qty is required"
-    });
+      'code': 'ValidationError',
+      'message': 'qty is required'
+    })
   }
-  if(!req.body && !req.body.value) {
+  if (!req.body && !req.body.value) {
     return res.status(400).json({
-      "code": "ValidationError",
-      "message": "value is required"
-    });
+      'code': 'ValidationError',
+      'message': 'value is required'
+    })
   }
-
 
   commerceService.createCreditMemo({
-    orderId : req.params.orderId,
-    qty : req.body.qty,
-    value : req.body.value
-  }, function(err, data){
-    if(err) return handleError(res, err);
-    return res.status(200).json(data);
-  });
+    orderId: req.params.orderId,
+    qty: req.body.qty,
+    value: req.body.value
+  }, function (err, data) {
+    if (err) return handleError(res, err)
+    return res.status(200).json(data)
+  })
 }
 
-exports.createShipment = function(req, res) {
+exports.createShipment = function (req, res) {
   var outputPromise = commerceService.createShipment(req.body.orderList)
-      .then(function (value) {
-        return res.status(200).json(value);
-      });
+    .then(function (value) {
+      return res.status(200).json(value)
+    })
 }
 
-function handleError(res, err) {
-  var httpErrorCode = 500;
+function handleError (res, err) {
+  var httpErrorCode = 500
 
-  if(err.name === "ValidationError") {
-    httpErrorCode = 400;
+  if (err.name === 'ValidationError') {
+    httpErrorCode = 400
   }
-  logger.log('error', err);
+  logger.log('error', err)
 
-  return res.status(httpErrorCode).json({code : err.name, message : err.message, errors : err.errors});
+  return res.status(httpErrorCode).json({code: err.name, message: err.message, errors: err.errors})
 }
