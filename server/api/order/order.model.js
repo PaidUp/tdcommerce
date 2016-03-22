69012433da7e60181ba864cf7cc0a1844094c5c0
Jesse Cogollo <cogollo87@gmail.com>
2016-03-22 15:39:16 -0500

    add createAt order model

--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -1,29 +1,31 @@
-'use strict';
+'use strict'
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 let paymentPlan = require('./paymentPlan/paymentPlan.model').paymentPlanSchema
 
-//TODO order machine with structure.
+// TODO order machine with structure.
 let orderObject = {
   status: {
-	type:String,
-	default:'pending',
-	enum: ['pending', 'complete', 'cancel', 'processing'],
-	lowercase: true
+    type: String,
+    default: 'pending',
+    enum: ['pending', 'complete', 'cancel', 'processing'],
+    lowercase: true
   },
   paymentsPlan: {
-	type:[paymentPlan],
-	default:[]
+    type: [paymentPlan],
+    default: []
   },
   userId: {
-	type: String
-  }
+    type: String
+  },
+  createAt: {type: Date, default: new Date()},
+  updateAt: {type: Date, default: new Date()}
 }
 
-let orderSchema =  new mongoose.Schema(orderObject)
-orderSchema.set('toObject', { virtuals:true})
-orderSchema.set('toJSON', { virtuals:true})
+let orderSchema = new mongoose.Schema(orderObject)
+orderSchema.set('toObject', { virtuals: true})
+orderSchema.set('toJSON', { virtuals: true})
 
-module.exports = orderObject// change for machine
+module.exports = orderObject // change for machine
 module.exports.orderSchema = orderSchema
 module.exports.orderModel = mongoose.model('order', orderSchema, 'orders')
