7b034a530262dd7e6b8d9ccb878807cbed830325
Jesse Cogollo <cogollo87@gmail.com>
2016-03-23 17:53:08 -0500

    add method cronjob orders

--- server/api/order/index2.js
+++ server/api/order/index2.js
@@ -2,13 +2,15 @@
 
 let express = require('express')
 let controller = require('./order.controller')
+let authServer = require('../auth/auth.service')
 
 let router = express.Router()
 
-router.post('/create', controller.create)
-router.post('/list', controller.listV2)
-router.put('/update', controller.update)
-router.post('/add-payments', controller.addPayments)
-router.post('/update-payments', controller.updatePayments)
+router.post('/create', authServer.isAuthenticated(), controller.create)
+router.post('/list', authServer.isAuthenticated(), controller.listV2)
+router.post('/cronjob', authServer.isAuthenticated(), controller.listCronjob)
+router.put('/update', authServer.isAuthenticated(), controller.update)
+router.post('/add-payments', authServer.isAuthenticated(), controller.addPayments)
+router.post('/update-payments', authServer.isAuthenticated(), controller.updatePayments)
 
 module.exports = router
--- server/api/order/order.controller.js
+++ server/api/order/order.controller.js
@@ -18,7 +18,15 @@ exports.create = function (req, res) {
 }
 
 exports.listV2 = function (req, res) {
-  orderModel.find(req.body, 'paymentsPlan userId' , function (err, orders) {
+  orderModel.find(req.body, function (err, orders) {
+    if (err) return res.status(400).json({err: err})
+    return res.status(200).json({orders: orders})
+  })
+}
+
+exports.listCronjob = function (req, res) {
+  let filter = {'paymentsPlan': { '$elemMatch': {'status': 'pending','dateCharge': {'$lte': new Date()}}},'status': 'pending'}
+  orderModel.find(filter, 'paymentsPlan.$ userId' , function (err, orders) {
     if (err) return res.status(400).json({err: err})
     return res.status(200).json({orders: orders})
   })
