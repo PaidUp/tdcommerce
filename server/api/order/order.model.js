f79cb44ed47da36717be6ee2768ef25bd02ad279
Ricardo Lara <riclara@gmail.com>
2016-04-05 15:36:06 -0500

    define seq

--- server/api/order/order.model.js
+++ server/api/order/order.model.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 let paymentPlan = require('./paymentPlan/paymentPlan.model').paymentPlanSchema
 
 // TODO order machine with structure.
+let seq;
 let orderObject = {
+  //orderNumber: {type: String, default: seq},
   status: {
     type: String,
     default: 'pending',
@@ -22,7 +24,34 @@ let orderObject = {
   updateAt: {type: Date, default: new Date()}
 }
 
+function getNextSequence(cb){
+  mongoose.connection.db.eval("getNextSequence('order_seq')", function(err, retVal) {
+    console.log('ERR---', err)
+    console.log('retVal', retVal)
+    if(err){
+      return cb(err)
+    }
+    seq = retVal;
+    return cb(null, retVal);
+  });
+
+}
+
+
+
 let orderSchema = new mongoose.Schema(orderObject)
+
+/*
+orderSchema.pre('save', function (next) {
+  getNextSequence(function(err, data){
+    if(err){
+      next(new Error('something went wrong'));
+    }
+    next();
+  });
+
+});
+*/
 orderSchema.set('toObject', { virtuals: true})
 orderSchema.set('toJSON', { virtuals: true})
 
