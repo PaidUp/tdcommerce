d7a0848920a84358ccb4402e5253a926db88134a
Ricardo Lara <riclara@gmail.com>
2017-08-02 20:11:36 -0400

    fix invoice

--- server/api/order/paymentPlan/paymentPlan.model.js
+++ server/api/order/paymentPlan/paymentPlan.model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 let paymentPlanObject = {
   // discount
-  invoiceId: {type: String, default: '', required: true},
+  invoiceId: {type: String, default: ''},
   version: {type: String, required: true},
   destinationId: {type: String, required: true},
   email: {type: String, required: true},
